871111f04338553906c54a5f898add92f20ad0f0
Marshall Bockrath-Vandegrift <llasram@damballa.com>
2014-01-26 09:09:56 -0500

    Initial commit of working code.

---
+++ src/lein_hadoop_cluster/plugin.clj
@@ -0,0 +1,43 @@
+(ns lein-hadoop-cluster.plugin
+  (require [clojure.java.io :as io]
+           [clojure.java.shell :refer [sh]]
+           [leiningen.core.project :as lcp]))
+
+(def hadoop-dependencies
+  '[org.apache.hadoop/hadoop-common
+    org.apache.hadoop/hadoop-client
+    org.apache.hadoop/hadoop-core
+    org.apache.hadoop/hadoop-hdfs
+    org.slf4j/slf4j-api
+    org.slf4j/slf4j-log4j12
+    log4j/log4j])
+
+(def hadoop-classpaths
+  (-> (sh "hadoop" "classpath")
+      :out (.split ":") (->> (map #(.trim %)))
+      vec))
+
+(def hadoop-cluster-profile
+  (cond-> {:exclusions hadoop-dependencies
+           :plugins '[[lein-extend-cp "0.1.0"]]
+           :lein-extend-cp {:paths hadoop-classpaths}}
+          (.exists (io/file "/usr/lib/hadoop/lib/native"))
+          , (assoc :jvm-opts ["-Djava.library.path=/usr/lib/hadoop/lib/native"])))
+
+(defn inject-profile
+  [project pname profile]
+  (-> (assoc-in project [:profiles pname] profile)
+      (assoc ::hadoop-cluster true)))
+
+(defn middleware
+  [project]
+  (if (::hadoop-cluster project)
+    project
+    (let [pwop (:without-profiles (meta project) project)
+          profiles (:profiles pwop {})
+          pname (:hadoop-cluster-profile pwop :hadoop-cluster)
+          profile (->> (get profiles pname {})
+                       (#'lcp/meta-merge hadoop-cluster-profile))]
+      (with-meta (inject-profile project pname profile)
+        (assoc (meta project)
+          :without-profiles (inject-profile pwop pname profile))))))
---
+++ src/leiningen/hadoop_cluster.clj
@@ -0,0 +1,6 @@
+(ns leiningen.hadoop-cluster)
+
+(defn hadoop-cluster
+  "I don't do a lot."
+  [project & args]
+  (println "Hi!"))
