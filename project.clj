9c8af7cbbd222b18952e58d19997e4012cef8115
Marshall Bockrath-Vandegrift <llasram@damballa.com>
2014-01-27 13:34:30 -0500

    Begin work on next iteration.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-hadoop-cluster "0.1.0"
+(defproject lein-hadoop-cluster "0.1.1-SNAPSHOT"
   :description "Run Leiningen tasks against a live Hadoop cluster."
   :url "http://github.com/llasram/lein-hadoop-cluster"
   :license {:name "Eclipse Public License"
