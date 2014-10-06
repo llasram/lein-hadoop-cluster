cf3b782066299eda970029c64d103109188d1d7b
Marshall Bockrath-Vandegrift <llasram@damballa.com>
2014-10-06 16:47:55 -0400

    Release version 0.1.3.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-hadoop-cluster "0.1.3-SNAPSHOT"
+(defproject lein-hadoop-cluster "0.1.3"
   :description "Run Leiningen tasks against a live Hadoop cluster."
   :url "http://github.com/llasram/lein-hadoop-cluster"
   :license {:name "Eclipse Public License"
