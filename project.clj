4dd00581abf3c0ea7cd6c5c1717bddc1a39bab31
Marshall Bockrath-Vandegrift <llasram@damballa.com>
2014-12-18 15:02:58 -0500

    Use new project key for plugin evaluation.

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "http://github.com/llasram/lein-hadoop-cluster"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in :leiningen)
